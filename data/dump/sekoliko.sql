338de0685964012e203f7b6990f8f131cfb895b8
julkwel <julienrajerison5@gmail.com>
2018-10-08 08:35:22 +0300

    suite changement prefixe

--- data/dump/sekoliko.sql
+++ data/dump/sekoliko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 07, 2018 at 02:32 PM
+-- Generation Time: Oct 08, 2018 at 07:33 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 7.2.5
 
@@ -37,7 +37,8 @@ CREATE TABLE `migration_versions` (
 --
 
 INSERT INTO `migration_versions` (`version`) VALUES
-('20181007033629');
+('20181007033629'),
+('20181008053051');
 
 -- --------------------------------------------------------
 
@@ -105,7 +106,6 @@ INSERT INTO `sekoliko_role` (`id`, `rl_name`) VALUES
 
 CREATE TABLE `sekoliko_user` (
   `id` int(11) NOT NULL,
-  `sekoliko_role_id` int(11) DEFAULT NULL,
   `username` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `username_canonical` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
@@ -132,10 +132,11 @@ CREATE TABLE `sekoliko_user` (
 -- Dumping data for table `sekoliko_user`
 --
 
-INSERT INTO `sekoliko_user` (`id`, `sekoliko_role_id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`, `usr_firstname`, `usr_lastname`, `usr_address`, `usr_date_create`, `usr_date_update`, `usr_phone`, `usr_photo`, `usr_raison_sociale`, `usr_color`) VALUES
-(10, 5, 'julien@techzara.mg', 'julien@techzara.mg', 'julien@techzara.mg', 'julien@techzara.mg', 1, NULL, '$2y$13$ttG0zf1MEFOgYkaBVwspGubh.29yqoD587M2NK/n.zW/4bONwM.Hm', '2018-10-07 14:31:49', NULL, NULL, 'a:1:{i:0;s:14:\"ROLE_PERSONNEL\";}', 'Jul', 'RAJERISON', 'Test', '2018-09-10 16:03:40', '2018-10-07 12:24:43', '0345987215', NULL, NULL, '#FF00FF'),
-(15, NULL, 'jul@jul.fr', 'jul@jul.fr', 'jul@jul.fr', 'jul@jul.fr', 1, NULL, '$2y$13$R1.Jpa1Dk3.7tgc527XViOrynCY28AYqIkHHaQLtsztCaZemcrXnC', NULL, NULL, NULL, 'a:0:{}', 'qqqqwqw', 'qqwq', 'qwq', '2018-10-07 07:19:57', '2018-10-07 07:37:43', '0329473033', NULL, NULL, '#FFFFFF'),
-(16, NULL, 'julien123@livenexx.fr', 'julien123@livenexx.fr', 'julien@julien.fr', 'julien@julien.fr', 1, NULL, '$2y$13$9rgRq3g.12OsagSwnpxf..oX4iOBpDrozmWaqR5cNQSCkCDBEcDEG', '2018-10-07 13:11:21', NULL, NULL, 'a:0:{}', 'Julien', 'Julien', 'julien', '2018-10-07 07:43:33', '2018-10-07 12:48:10', '0329473033', NULL, NULL, '#FFFFFF');
+INSERT INTO `sekoliko_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `confirmation_token`, `password_requested_at`, `roles`, `usr_firstname`, `usr_lastname`, `usr_address`, `usr_date_create`, `usr_date_update`, `usr_phone`, `usr_photo`, `usr_raison_sociale`, `usr_color`) VALUES
+(10, 'julien@techzara.mg', 'julien@techzara.mg', 'julien@techzara.mg', 'julien@techzara.mg', 1, NULL, '$2y$13$ttG0zf1MEFOgYkaBVwspGubh.29yqoD587M2NK/n.zW/4bONwM.Hm', '2018-10-07 20:28:42', NULL, NULL, 'a:1:{i:0;s:14:\"ROLE_PERSONNEL\";}', 'Jul', 'RAJERISON', 'Test', '2018-09-10 16:03:40', '2018-10-07 12:24:43', '0345987215', NULL, NULL, '#FF00FF'),
+(15, 'jul@jul.fr', 'jul@jul.fr', 'jul@jul.fr', 'jul@jul.fr', 1, NULL, '$2y$13$R1.Jpa1Dk3.7tgc527XViOrynCY28AYqIkHHaQLtsztCaZemcrXnC', NULL, NULL, NULL, 'a:0:{}', 'qqqqwqw', 'qqwq', 'qwq', '2018-10-07 07:19:57', '2018-10-07 07:37:43', '0329473033', NULL, NULL, '#FFFFFF'),
+(16, 'julien123@livenexx.fr', 'julien123@livenexx.fr', 'julien@julien.fr', 'julien@julien.fr', 1, NULL, '$2y$13$9rgRq3g.12OsagSwnpxf..oX4iOBpDrozmWaqR5cNQSCkCDBEcDEG', '2018-10-07 13:11:21', NULL, NULL, 'a:0:{}', 'Julien', 'Julien', 'julien', '2018-10-07 07:43:33', '2018-10-07 12:48:10', '0329473033', NULL, NULL, '#FFFFFF'),
+(17, 'Julien', 'julien', 'julienrajerison5@gmail.com', 'julienrajerison5@gmail.com', 1, NULL, '$2y$13$lBE7yUpM4pt8QxG5PfqkjOFfbByH7yHOta.frmJ7VF.XPJKw9v3Ge', NULL, NULL, NULL, 'a:0:{}', 'Julien', 'RAJERISON', 'Antananarivo', '2018-10-08 07:27:03', NULL, '032 94 730 33', '/upload/user/555a46df6a04a4083a4849801e68ea0c.jpg', NULL, '#FFFF');
 
 --
 -- Indexes for dumped tables
@@ -172,8 +173,7 @@ ALTER TABLE `sekoliko_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username_canonical_UNIQUE` (`username_canonical`),
   ADD UNIQUE KEY `email_canonical_UNIQUE` (`email_canonical`),
-  ADD UNIQUE KEY `confirmation_token_UNIQUE` (`confirmation_token`),
-  ADD KEY `IDX_FD4DE842FAF82993` (`sekoliko_role_id`);
+  ADD UNIQUE KEY `confirmation_token_UNIQUE` (`confirmation_token`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -201,7 +201,7 @@ ALTER TABLE `sekoliko_role`
 -- AUTO_INCREMENT for table `sekoliko_user`
 --
 ALTER TABLE `sekoliko_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
